f7293eb53f4476df64ddc3a6cfa03d59403893f2
Centro de Servicios Unibague <servicios@unibague.edu.co>
2025-10-24 15:40:21 -0500

    :sparkles: Se crea base del proyecto

---
+++ backend/contratos_app/database/migrations/2025_10_24_195358_create_contracts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 120);
+            $table->string('contractor_name', 120);
+            $table->string('status', 10);
+            $table->float('amount', 10, 2);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
